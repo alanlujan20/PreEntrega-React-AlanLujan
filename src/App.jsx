e8bfe6a9c9610f009b8a75e5a9c38c557bfbf8a9
Alan Lujan <122304302+alanlujan20@users.noreply.github.com>
2023-12-01 01:17:40 -0300

    "actualización de firebase + arreglos"

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import Contacto from "./components/Contacto";
+
 import ItemDetailContainer from "./components/ItemDetailContainer";
 import ItemListContainer from "./components/ItemListContainer";
 import Navbar from "./components/Navbar";
-import Nosotros from "./components/Nosotros";
 import "./main.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
@@ -22,8 +21,6 @@ function App() {
             <Route path="/item/:id" element={<ItemDetailContainer />}/>
             <Route path="/productos" element={<ItemListContainer />} />
             <Route path="/productos/:categoria" element={<ItemListContainer />} />
-            <Route path="/nosotros" element={<Nosotros />}/>
-            <Route path="/contacto" element={<Contacto />}/>
             <Route path="/carrito" element={<Carrito />}/>
             <Route path="/checkout" element={<Checkout />}/>
           </Routes>
